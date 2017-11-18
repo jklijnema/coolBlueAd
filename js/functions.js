a2ac018a217b1b7710853eca0fa23bd0e63b1895
Jorrik Klijnsma <jklijnema@hotmail.nl>
2017-11-18 09:44:57 +0100

    First commit

--- js/functions.js
+++ js/functions.js
@@ -1,45 +1,62 @@
 /**
  * Created by jorrikklijnsma on 17/11/2017.
  */
-var ad = document.getElementsByClassName("ad")[0];
 var windowRotate = document.getElementsByClassName("rotateIt")[0];
 var timeoutLoop = 0;
+var timeoutFaster = 0;
+var frameRate = 60;
+var frameTime = (1000 / frameRate);
+var deg = 0;
+var animTime = 7500;
+var initTime = 7500;
+var fasterCounter = 0;
 
 function OnMouseIn() {
-	var i = 1;                     //  set your counter to 1
-	var delay =10 / i;
-	
-	
-	
-	function rotateLoop() {           //  create a loop function
-		//delay = 200 / i;
-		var seconds = 7.5 - (.075 * i);
-		if (seconds < .75) {
-			seconds = .75;
-		}
-		windowRotate.setAttribute("style","animation: rotate "+seconds+"s linear infinite;");
-		
-		console.log(i);
-		i++;
-		
-		timeoutLoop = setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-			//  increment the counter
-			if (i < 100) {            //  if the counter < 10, call the loop function
-				rotateLoop();             //  ..  again which will trigger another
-			}                 //  ..  setTimeout()
-		}, delay)
-	}
-	
-	timeoutLoop = setTimeout(rotateLoop, delay);
+	timeoutFaster = setTimeout(fasterAnim, frameTime);
 }
 
 function OnMouseOut() {
-	if (timeoutLoop) {
-		clearTimeout(timeoutLoop);
-		timeoutLoop = 0;
-		windowRotate.setAttribute("style","animation: rotate 7.5s linear infinite;");
-	}
+	console.log(1);
+	clearTimeout(timeoutFaster);
 }
 
 window.onload = function(e){
+	timeoutLoop = setTimeout(rotateLoop, frameTime);
+}
+
+function fasterAnim() {
+	var animPart;
+	
+	if (fasterCounter < 60) {
+		animPart = initTime / ((60 - fasterCounter));
+		animTime = initTime - (animPart);
+		//console.log(animPart);
+	}
+	
+	if (animTime < 750) {
+		animTime = 750;
+	}
+	
+	
+	console.log(animTime);
+	
+	fasterCounter ++;
+	timeoutFaster = setTimeout(fasterAnim, frameTime);
+}
+
+function rotateLoop() {
+	calcDeg();
+	//console.log(deg);
+	windowRotate.setAttribute("style","transform: rotate("+deg+"deg);");
+	
+	timeoutLoop = setTimeout(rotateLoop, frameTime);
+}
+
+function calcDeg() {
+	var animDuration = animTime / frameTime;
+	var degPart = 360 / animDuration;
+	deg = deg + degPart;
+	if (deg > 360) {
+		deg = deg - 360;
+	}
 }
